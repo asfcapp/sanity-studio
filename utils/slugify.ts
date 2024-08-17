686b749dfb3a16716734ac100a5e43a6efdbe5c6
asfcapp <asfcapp@gmail.com>
2024-08-17 03:46:36 +0100

    Update slugify.ts

--- utils/slugify.ts
+++ utils/slugify.ts
@@ -1,16 +1,15 @@
 /**
- * Slugify has for purpose to converting it into a URL-friendly format.
+ * Slugifies a given text string, converting it into a URL-friendly format.
  *
  * @param text The input text to be slugified.
  * @returns The slugified text.
  */
 export function slugify(text: string): string {
   return text
-    .toLowerCase()
-    .normalize('NFD') // Normalize to remove accents
-    .replace(/[\u0300-\u036f]/g, '') // Remove remaining accents
-    .replace(/[^a-z0-9-]/g, '') // Remove non-alphanumeric characters and hyphens
-    .trim()
-    .replace(/\s+/g, '-')
+    .toLowerCase() // Convert to lowercase for consistency
+    .normalize('NFD') // Normalize to decompose characters with accents into their base characters and combining marks
+    .replace(/[\u0300-\u036f]/g, '') // Remove combining marks, which represent accents
+    .replace(/[^a-z0-9-]/g, '') // Remove any characters that are not lowercase letters, numbers, or hyphens
+    .trim() // Remove leading and trailing whitespace
+    .replace(/\s+/g, '-'); // Replace multiple consecutive whitespace characters with a single hyphen
 }
-//Should be placed in utils and imported in schemas
