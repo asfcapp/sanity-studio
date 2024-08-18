56e42e9617ab04a43463c9c0d5bfc4aace186f3f
asfcapp <asfcapp@gmail.com>
2024-08-18 14:10:40 +0100

    Update partenaire.ts

--- schemaTypes/partenaire.ts
+++ schemaTypes/partenaire.ts
@@ -1,29 +1,73 @@
-import { ComponentIcon } from '@sanity/icons';
-
+// partnerReference.js
 export default {
-  name: 'partenaire',
+  name: 'partnerReference',
   type: 'document',
-  title: 'Partenaires',
-  icon: ComponentIcon,
+  title: 'Partner Reference',
   fields: [
-    {
-      name: 'photo',
-      type: 'image',
-      title: 'Photo',
-      options: {
-        hotspot: true, // Enables the user to specify the hotspot for the image
-      },
-    },
-    {
-      name: 'name',
-      type: 'string',
-      title: 'Name',
-    },
-    {
-      name: 'review',
-      type: 'text',
-      title: 'Review',
-    },
-   
+    // ... fields for partner information
   ],
 };
+
+// A generic partner document. ROJ and Campaign schemas will specify if they need this partner.
+
+// roj.js
+export default {
+  name: 'roj',
+  type: 'document',
+  title: 'ROJ',
+  fields: [
+    {
+      name: 'partners',
+      type: 'array',
+      title: 'Partners',
+      of: [{ type: 'reference', to: { type: 'partnerReference' } }],
+      // A ROJ can have multiple partners.
+    },
+    // ... other ROJ fields
+  ],
+};
+
+// campaign.js
+export default {
+  name: 'campaign',
+  type: 'document',
+  title: 'Campaign',
+  fields: [
+    {
+      name: 'partners',
+      type: 'array',
+      title: 'Partners',
+      of: [{ type: 'reference', to: { type: 'partnerReference' } }],
+      // A campaign can have multiple partners.
+    },
+    // ... other campaign fields
+  ],
+};
+
+// Example of how ROJ and Campaign might use partnerReference:
+
+// ROJ:
+// {
+//   name: "ROJ 1",
+//   partners: [
+//     {
+//       _ref: "partnerReferenceId1" // Reference to a specific partner
+//     },
+//     {
+//       _ref: "partnerReferenceId2" // Reference to another partner
+//     }
+//   ]
+// }
+
+// Campaign:
+// {
+//   name: "Campaign A",
+//   partners: [
+//     {
+//       _ref: "partnerReferenceId2" // Reference to a partner also used in ROJ 1
+//     },
+//     {
+//       _ref: "partnerReferenceId3" // Reference to a different partner
+//     }
+//   ]
+// }
