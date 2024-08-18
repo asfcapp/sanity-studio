027ba7ec6d07fac26e9f931039cf22740050d9d0
asfcapp <asfcapp@gmail.com>
2024-08-18 16:31:31 +0100

    Update image.ts

--- schemaTypes/image.ts
+++ schemaTypes/image.ts
@@ -1,49 +1,59 @@
-import {defineType} from 'sanity'
+import { defineType, ComponentIcon } from 'sanity'
 
 export default defineType({
   name: 'image',
   title: 'Image',
   type: 'image',
+  options: {
+    hotspot: true, // Enable hotspot functionality for cropping and positioning
+  },
   fields: [
     {
       name: 'caption',
       type: 'string',
       title: 'Caption',
-      // This field is used to provide additional context or description for the image.
+      // Optional field for providing additional context or description
     },
     {
       name: 'alt',
       type: 'string',
       title: 'Alternative text',
-      validation: (Rule) => Rule.required(), // Make alt text required for accessibility
-      // This field is essential for accessibility. It provides a textual description of the image for users who cannot see it, such as visually impaired users or users with slow internet connections.
+      validation: (Rule) => Rule.required(), // Make alt text mandatory for accessibility
+      // Essential for describing the image for visually impaired users or users with slow connections
     },
     {
       name: 'credit',
       type: 'string',
       title: 'Credit',
-      // This field is used to attribute the image to its creator or source.
+      // Optional field for attributing the image to its creator or source
     },
     {
       name: 'asset',
       title: 'Asset',
       type: 'reference',
-      to: [{type: 'asset'}], // Assuming you have an asset type for managing image files
-      // This field references an asset document, which might contain additional information about the image, such as its original file, dimensions, or metadata.
+      to: [{ type: 'asset' }],
+      // References an asset document, which might contain additional information about the image
     },
     {
       name: 'width',
       title: 'Width',
       type: 'number',
-      hidden: true, // Hide from the studio interface
-      // Comment: This field stores the width of the image in pixels. It's automatically generated and hidden from the studio interface.
+      hidden: true,
+      // Automatically generated field storing the image width in pixels
     },
     {
       name: 'height',
       title: 'Height',
       type: 'number',
       hidden: true,
-      // Comment: This field stores the height of the image in pixels. It's automatically generated and hidden from the studio interface.
+      // Automatically generated field storing the image height in pixels
+    },
+    {
+      name: 'icon',
+      type: 'image',
+      title: 'Icon',
+      // Use a built-in icon from @sanity/icons
+      icon: ComponentIcon.Image,
     },
   ],
 })
