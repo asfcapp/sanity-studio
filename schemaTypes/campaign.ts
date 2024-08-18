0bee62ce010b685271f3580b980107974145e307
asfcapp <asfcapp@gmail.com>
2024-08-18 18:15:05 +0100

    Update campaign.ts

--- schemaTypes/campaign.ts
+++ schemaTypes/campaign.ts
@@ -1,85 +1,53 @@
-//MUST ADD REFERENCE TO PARTENAIRES
 import { defineType } from 'sanity';
 import { AddDocumentIcon } from '@sanity/icons';
 import content from './content'; // Importing the content schema to inherit fields
 
 export default defineType({
   name: 'campagne',
-  title: 'Campagne',
+  title: 'Campaign',
   type: 'document',
   icon: AddDocumentIcon,
   fields: [
     // Inherit fields from the content schema, allowing for selective inheritance.
     // This means we can choose exactly which fields from 'content' we want to include in this schema.
-    //This means also we can customize/add new fields even if they are not in content.ts (LSP principle)
+    // This also allows for customization of fields even if they are not in content.ts (LSP principle)
     ...content.fields,
 
     // Campaign-specific fields
     {
+      name: 'name',
+      title: 'Campaign Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+     // This field is required. It must be filled in for each campaign.
+    },
+    {
       name: 'description',
-      title: 'Brève Présentation',
+      title: 'Brief Description',
       type: 'text',
       validation: (Rule) => Rule.required().max(200),
-      // A brief description of the campaign, providing a quick overview of its purpose and scope.
-      // This field is required and limited to 200 characters to ensure a concise summary.
+      // A concise overview of the campaign
     },
     {
-      name: 'partenaires',
-      title: 'Partenaires',
+      name: 'partners',
+      title: 'Partners',
       type: 'array',
       of: [{ type: 'reference', to: [{ type: 'partners' }] }],
-      // An array of references to Partner documents.
-      // This allows for linking multiple partners to the campaign, enabling flexible and reusable partner associations.
-    },
-    {
-      name: 'donnees',
-      title: 'Données de la Campagne',
-      type: 'array',
-      of: [
-        {
-          name: 'dataField',
-          title: 'Champ de données',
-          type: 'object',
-          fields: [
-            {
-              name: 'name',
-              title: 'Nom du champ',
-              type: 'string',
-              validation: (Rule) => Rule.required(),
-              // A required string field for the name of the data field.
-              // It identifies each metric or specific data point related to the campaign.
-            },
-            {
-              name: 'value',
-              title: 'Valeur du champ',
-              type: 'string',
-              // A string field for the value associated with the data field.
-              // Allows capturing various campaign-specific information flexibly.
-            },
-          ],
-        },
-      ],
-      // A flexible array of data fields for campaigns.
-      // Enables dynamic and customizable campaign-specific data to be captured.
+      // An array of references to Partner documents for collaboration
     },
     {
       name: 'infractions',
       title: 'Infractions',
       type: 'array',
       of: [{ type: 'reference', to: [{ type: 'infraction' }] }],
-      // An array of references to Infraction documents.
-      // This allows associating specific infractions with the campaign, aiding in the management of related offenses.
+      // An array of references to Infraction documents associated with the campaign
     },
     {
       name: 'seo',
       title: 'SEO',
       type: 'reference',
       to: [{ type: 'seo' }],
-      // Reference to the SEO schema for optimizing the campaign for search engines.
-      // Ensures that campaigns are properly configured to improve online visibility.
+      // Reference to SEO data for optimizing the campaign's search visibility
     },
   ],
 });
-
-
-
