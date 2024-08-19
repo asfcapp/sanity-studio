3e0655e4c1e134a2010f2cb2d531ac1a9053e1c0
youssef-mous <106970977+youssef-mous@users.noreply.github.com>
2024-08-19 18:27:10 +0100

    add the newsfeed

---
+++ schemaTypes/newsFeed.ts
@@ -0,0 +1,18 @@
+import {defineType} from 'sanity'
+
+// Simplified validation function to ensure all referenced items have 'isDisplayedOnHome' set to true
+function validateNewsfeed(value, context) {
+  return value.every((ref) => context.get(ref._ref)?.isDisplayedOnHome === true)
+    ? true
+    : 'All referenced content must have isDisplayedOnHome set to true'
+}
+
+// Define the 'Newsfeed' schema
+export default defineType({
+  name: 'newsfeed',
+  title: 'Newsfeed',
+  type: 'array',
+  of: [{type: 'reference', to: [{type: 'blog'}, {type: 'communique'}, {type: 'ressource'}]}],
+  // Apply the validation function to the 'newsfeed' field
+  validation: validateNewsfeed,
+})
--- schemaTypes/partners.ts
+++ schemaTypes/partners.ts
@@ -1,5 +1,5 @@
-import { defineType } from 'sanity';
-import content from './content'; // Importing the content schema
+import {defineType} from 'sanity'
+import content from './content' // Importing the content schema
 
 // Define the Partner document type
 export default defineType({
@@ -14,23 +14,20 @@ export default defineType({
     ...content.fields,
 
     // Specific fields for Partner documents
-    //Might need its proper descrption 
+    //Might need its proper descrption
     {
       // Partner type selection - using checkboxes for multiple selections
       name: 'partnerType',
       title: 'Partner Type',
       type: 'array', // Array to store multiple selections
-      of: [{ type: 'string' }], // Each element in the array will be a string
+      of: [{type: 'string'}], // Each element in the array will be a string
       options: {
         list: ['ROJ', 'Campagne'], // Predefined options for selection
         layout: 'checkbox', // Display options as checkboxes (allows multiple selections)
       },
       initialValue: [], // Set an empty array as the default value (no selections)
-      validation: (Rule) =>
-        Rule.required() // Partner type is required (at least one checkbox must be selected)
-          // No need for min or max validation since checkboxes allow any number of selections
+      validation: (Rule) => Rule.required(), // Partner type is required (at least one checkbox must be selected)
+      // No need for min or max validation since checkboxes allow any number of selections
     },
-    {
-    
-    {
-     
+  ],
+})
