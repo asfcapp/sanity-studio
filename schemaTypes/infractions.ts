fc4c37b49bb17ee7a85c5989409034c66f2a04df
youssef-mous <106970977+youssef-mous@users.noreply.github.com>
2024-08-19 12:27:58 +0100

    Update infractions.ts

--- schemaTypes/infractions.ts
+++ schemaTypes/infractions.ts
@@ -22,29 +22,15 @@ export default defineType({
     {
       name: 'lawArticle',
       title: 'Law Article',
-      type: 'string',
+      type: 'blockContent',
       // Reference to the relevant article in the legal code
     },
     {
       name: 'moroccanData',
       title: 'Moroccan Data',
       type: 'array',
-      of: [{ type: 'string' }], // Adjust the type based on the specific data you need
+      of: [{ type: 'blockContent' }], // Adjust the type based on the specific data you need
       // Specific data related to the offense within the Moroccan context
-    },
-    {
-      name: 'publications',
-      title: 'Publications',
-      type: 'array',
-      of: [{ type: 'file' }], // Adjust the type to 'reference' if you want to reference existing publication documents
-      // Relevant documents (like PDFs) related to the offense
-    },
-    {
-      name: 'campaign',
-      title: 'Campaign',
-      type: 'reference',
-      to: [{ type: 'campaign' }],
-      // The campaign to which this infraction belongs
     },
   ],
 });
