a38da43b80866fd0a0a27c0484146d5310633a44
asfcapp <asfcapp@gmail.com>
2024-08-20 13:26:36 +0100

    Update index.ts

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,19 +1,20 @@
 // Import your schemas
-import blockContent from './blockContent'
-import content from './content'
-import {author} from './author'
-import tag from './tag'
-import {roj} from './roj'
-import partner from './partners'
-import aboutUs from './aboutUs'
-import newsfeed from './newsFeed'
-import blog from './blogs'
-import campagne from './campaign'
-import infraction from './infractions'
-import communique from './communiques'
-import footer from './footer'
-// Export all  schemas
-export const documentSchemas = [
+import blockContent from './blockContent';
+import content from './content';
+import { author } from './author';
+import tag from './tag';
+import { roj } from './roj';
+import partner from './partners';
+import aboutUs from './aboutUs';
+import newsfeed from './newsFeed';
+import blog from './blogs';
+import campagne from './campaign';
+import infraction from './infractions';
+import communique from './communiques';
+import footer from './footer';
+
+// Export all schemas as 'schemaTypes'
+export const schemaTypes = [
   campagne,
   content,
   infraction,
@@ -27,4 +28,4 @@ export const documentSchemas = [
   aboutUs,
   newsfeed,
   footer,
-]
+];
